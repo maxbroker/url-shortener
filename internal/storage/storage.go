a8d9a58ffe0da2366cab426c700224ce2d892aea
maxbroker <camosval174@gmail.com>
2024-08-05 23:33:26 +0500

    Функция SaveUrl работает

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -13,14 +13,14 @@ type Storage struct {
 }
 
 type StorageFile struct {
-	id    int
-	alias string
-	url   string
+	ID    int
+	Alias string
+	URL   string
 }
 
-func New(CollectionName string) (*Storage, error) {
+func ConnectionToDB(CollectionName string) (*Storage, error) {
 
-	const op = "storage.mongoDB.New"
+	const op = "storage.mongoDB.ConnectionToDB"
 
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
@@ -40,27 +40,46 @@ func New(CollectionName string) (*Storage, error) {
 
 	collection := client.Database("mydb").Collection(CollectionName)
 
-	first_link := StorageFile{1, "alias", "https://www.youtube.com/watch?v=oEupPPSes2I&list=PLNkWIWHIRwMFJ-3-gI7GC5JDg1ivbIKNR&index=6"}
-
-	InsertOneResult, err := collection.InsertOne(context.TODO(), first_link)
-
-	if err != nil {
-		return nil, fmt.Errorf("%s: %v", op, err)
-	}
-
-	fmt.Println("Inserted a single document: ", InsertOneResult.InsertedID)
+	//first_link := StorageFile{1, "alias", "https://www.youtube.com/watch?v=oEupPPSes2I&list=PLNkWIWHIRwMFJ-3-gI7GC5JDg1ivbIKNR&index=6"}
+	//
+	//InsertOneResult, err := collection.InsertOne(context.TODO(), first_link)
+	//if err != nil {
+	//	return nil, fmt.Errorf("%s: %v", op, err)
+	//}
+	//
+	//fmt.Println("Inserted a single document: ", InsertOneResult.InsertedID)
 
 	first_collection := Storage{collection}
-
+	fmt.Printf("AAAA", first_collection.db)
 	return &first_collection, nil
 }
 
 func main() {
 	const op = "storage.main"
 
-	storage, err := New("url-shortener")
+	storage, err := ConnectionToDB("url-shortener")
 	if err != nil {
 		log.Fatalf("%s: %v", op, err)
 	}
 	_ = storage
+	id, err := storage.SaveUrl("https://www.youtube.com/watch?v=oEupPPSes2I&list=PLNkWIWHIRwMFJ-3-gI7GC5JDg1ivbIKNR&index=6", "alias")
+	if err != nil {
+		log.Fatalf("%s: %v", op, err)
+	}
+	_ = id
+}
+
+// first_link := StorageFile{1, "alias", "https://www.youtube.com/watch?v=oEupPPSes2I&list=PLNkWIWHIRwMFJ-3-gI7GC5JDg1ivbIKNR&index=6"}
+func (s *Storage) SaveUrl(UrlToSave string, alias string) (int64, error) {
+	const op = "storage.saveUrl"
+	collection := s.db
+	first_link := StorageFile{1, alias, UrlToSave}
+
+	InsertOneResult, err := collection.InsertOne(context.TODO(), first_link)
+	if err != nil {
+		return 1, fmt.Errorf("%s: %v", op, err)
+	}
+
+	fmt.Println("Inserted a single document: ", InsertOneResult.InsertedID)
+	return 1, nil
 }
